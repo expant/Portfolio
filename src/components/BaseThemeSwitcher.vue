c390693a4a36fa27c159650782cc4ebf43bf12e6
Expant <anton532202@gmail.com>
2024-09-12 16:51:55 +0300

    animate theme switcher

--- src/components/BaseThemeSwitcher.vue
+++ src/components/BaseThemeSwitcher.vue
@@ -3,24 +3,24 @@ import BaseIcon from './BaseIcon.vue';
 import { state } from '@/state.js';
 import gsap from 'gsap';
 
-// const animateSwitch = (theme) => {
-//   console.log(theme);
-//   gsap.fromTo('.switcher', 
-//     { 
-//       x: 0,
-//       justifyContent: theme === 'light' ? 'flex-start' : 'flex-end', 
-//     },
-//     {
-      
-//       justifyContent: theme === 'light' ? 'flex-end' : 'flex-start',
-//       duration: 0.5,
-//     }
-//   );
-// }
+const animateSwitch = (theme) => {
+  if (theme === 'dark') {
+    gsap.fromTo('.switcher', 
+      { x: 0 },
+      { x: 36, duration: 0.3 }
+    );
+    return;
+  }
+  gsap.fromTo('.switcher', 
+    { x: 36 },
+    { x: 0, duration: 0.3 }
+  );
+ 
+}
 
 const switchTheme = () => {
   state.theme = state.theme === 'light' ? 'dark' : 'light';
-  // animateSwitch(state.theme);
+  animateSwitch(state.theme);
 };
 
 const iconState = {
@@ -42,16 +42,40 @@ const iconState = {
 
 <template>
   <button 
-    :class="`switcher transition-all flex w-20 rounded-[25px] p-1 bg-transparent border ${state.theme === 'light' ? 'justify-start border-[#212121]' : 'justify-end border-white'}`" 
+    :class="[
+      'relative w-20 px-[10px] py-[10px] rounded-[25px] bg-transparent border', 
+      state.theme === 'light' ? 'justify-start border-[#212121]' : 'justify-end border-white',
+    ]"
     @click="switchTheme"
   >
-    <div :class="`flex justify-center items-center w-[40px] h-[36px] rounded-[20px] ${state.theme === 'light' ? 'bg-[#212121]' : 'bg-indigo-900'}`">
+    <div 
+      :class="[
+        'switcher absolute left-[1px] top-[1px] flex justify-center items-center w-[40px] h-[36px] rounded-[20px]', 
+        state.theme === 'light' ? 'bg-[#212121]' : 'bg-indigo-900',
+      ]"
+    >
       <BaseIcon
         :iconName="state.theme"
         :iconWidth="iconState[state.theme].width"
         :iconHeight="iconState[state.theme].height"
         :fill="iconState[state.theme].color"
         :hasStroke="iconState[state.theme].hasStroke"
+      />
+    </div>
+    <div class="flex justify-between items-center ps-1">
+      <BaseIcon
+        iconName="light"
+        iconWidth="18"
+        iconHeight="18"
+        fill="#FFFFFF"
+        :hasStroke="iconState.light.hasStroke"
+      />
+      <BaseIcon
+        iconName="dark"
+        :iconWidth="18"
+        :iconHeight="18"
+        fill="#212121"  
+        :hasStroke="iconState.dark.hasStroke"
       />
     </div>
   </button>
--- src/components/views/TheNavigation.vue
+++ src/components/views/TheNavigation.vue
@@ -7,13 +7,15 @@ const nav = ['Contact me', 'Projects', 'About'];
 const lightStyles = 'text-[#212121] hover:text-white hover:bg-[rgb(33,33,33)]';
 const darkStyles = 'text-white hover:text-[#212121] hover:bg-white';
 
+// uppercase text-8xl py-[20px] px-[20px] ${state.theme === 'light' ? lightStyles : darkStyles} transition-all
+
 </script>
 
 <template>
   <nav class="pt-[125px] pr-[120px] ml-auto">
     <ul class="flex flex-col items-end">
       <li
-        :class="`uppercase text-8xl py-[20px] px-[20px] ${state.theme === 'light' ? lightStyles : darkStyles} transition-all`"
+        :class="['uppercase text-8xl py-[20px] px-[20px] transition-all', state.theme === 'light' ? lightStyles : darkStyles]"
         v-for="item in nav"
         :key="item"
       >{{ item }}</li>
