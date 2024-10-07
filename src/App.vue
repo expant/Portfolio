9419e6a09f1dac24c848909a8b931ed7a1bfd86a
Expant <anton532202@gmail.com>
2024-10-07 19:50:58 +0300

    create projects

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,63 @@
 <script setup>
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import TheHeader from './components/views/TheHeader.vue';
 import TheNavigation from './components/views/TheNavigation.vue';
 import { state } from '@/state.js';
+import { onMounted } from 'vue';
 
 const { sections, sectionIsActive } = state;
 
-const switchSection = (event) => {
-  if (!sectionIsActive) {
-    return;
-  } 
+gsap.registerPlugin(ScrollTrigger)
+// gsap.defaults({ duration: 2 });
 
-  if (event.deltaY > 0) {
-    const first = sections.shift();
-    state.sections.push(first);
-    state.sectionIsActive = false;
-    setTimeout(() => {
-      state.sectionIsActive = true;
-    }, 3000);
-    console.log(state.sections);
-    return;
-  }
-  const last = sections.pop();
-  state.sections.unshift(last);
-  state.sectionIsActive = false;
-  setTimeout(() => {
-    state.sectionIsActive = true;
-  }, 3000);
-};
+onMounted(() => {
+  let sections = gsap.utils.toArray('.nav-item');
+  gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: 'none',
+    scrollTrigger: {
+      trigger: '.gsap-container',
+      pin: true,
+      scrub: 1,
+      snap: 1 / (sections.length - 1),
+      end: () => '+=' + document.querySelector('.gsap-container').offsetWidth,
+    },
+  });
+  
+});
 
 </script>
 
 <template>
   <div 
-    :class="`h-screen w-full ${state.theme === 'light' ? 'bg-white' : 'bg-[#212121]'}`"
-    @wheel="switchSection"
+    :class="[state.theme === 'light' ? 'bg-white' : 'bg-[#212121]']"
   >
-    <div class="p-[40px]">
-      <section v-if="sections[0] === 'nav'">
+    <div class="w-screen">
+      <section class="h-screen p-[40px]">
         <TheHeader />
         <TheNavigation />
       </section>
-      <section v-if="sections[0] === 'about'">
-        <div class="flex w-screen h-screen justify-center items-center text-7xl">
-          <span>about</span>
-        </div>
-      </section>
-      <section v-if="sections[0] === 'projects'">
-        <div class="flex w-screen h-screen justify-center items-center text-7xl">
+      <div class="gsap-container w-[300%] h-screen flex flex-nowrap">
+        <section class="nav-item flex w-full h-full justify-center items-center">
+          <div class="flex gap-[40px] max-w-[40%]">
+            <div class="text-right">
+              <div :class="['inline-block text-xl py-[5px] px-[10px] uppercase', state.theme === 'light' ? 'bg-[#212121] text-white' : 'bg-white text-[#212121]']">Elagin Anton, 24</div>
+              <div class="text-[#909090] text-xl">Russia, Tver</div>
+              <p class="text-[#909090] text-xl mt-[30px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur.
+              </p>
+            </div>
+            <img src="@/assets/avatar-rect.png" alt="Avatar">
+          </div>
+        </section>
+        <section class="nav-item flex w-full h-full justify-center items-center text-7xl text-white bg-sky-700">
           projects
-        </div>
-      </section>
-      <section v-if="sections[0] === 'contact me'">
-        <div class="flex w-screen h-screen justify-center items-center text-7xl">
+        </section>
+        <section class="nav-item w-full h-full  flex justify-center items-center text-7xl text-white bg-black">
           contact me
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   </div>
 </template>
