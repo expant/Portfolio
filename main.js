71803d54cc0aaba063487a6ce961d9dbf013ea95
Expant <anton532202@gmail.com>
2026-04-14 15:14:48 +0300

    feat: add project glitch

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ import infiniteMarquee from './src/modules/infiniteMarquee';
 import follower from './src/modules/follower';
 import textNodes from './src/modules/textNodes';
 import preloader from './src/modules/preloader';
+import projectGlitch from './src/modules/projectGlitch';
 
-document.addEventListener("DOMContentLoaded", () => {
-  preloader();
-  navigation();
-  infiniteMarquee();
-  follower()
-  textNodes();
-}); 
+document.addEventListener('DOMContentLoaded', () => {
+	preloader();
+	navigation();
+	infiniteMarquee();
+	follower();
+	textNodes();
+	projectGlitch();
+});
